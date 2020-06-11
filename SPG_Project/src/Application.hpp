edd6d545dad3cbcce2d0e48f76886760f64e9053
Enachi Vasile <vasile.enachi@student.tuiasi.ro>
2020-06-12 02:48:56 +0300

    Splited some functions into game files.

--- SPG_Project/src/Application.hpp
+++ SPG_Project/src/Application.hpp
@@ -12,11 +12,6 @@ void passiveMotion(int, int);
 void putChecker();
 void undo();
 
-int moveIsLegal(int);
-void listOfJumpes(std::list<std::pair<int, int>>& jumpList, std::list<std::pair<int, int>>& checkList);
-void listOfMoves(std::list<std::pair<int, int>>& moveList);
 
-bool noMoreMoves(int);
-int countCheckers(int);
 void drawPossibleMoves();
 
